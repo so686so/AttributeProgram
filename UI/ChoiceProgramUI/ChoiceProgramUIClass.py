d484a9cf6f1a4c5cf116cfb557620daec4814d8b
SoByungJun <so5063@naver.com>
2021-11-05 12:05:09 +0900

    Add ChoiceProgramUI

---
+++ UI/ChoiceProgramUI/ChoiceProgramUIClass.py
@@ -0,0 +1,114 @@
+"""
+어느 프로그램을 실행할지 고르는 UI
+
+LAST_UPDATE : 2021/11/05
+AUTHOR      : SO BYUNG JUN
+"""
+
+
+# Import Packages and Modules
+# Standard Library
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+import sys
+import os
+import copy
+
+
+# Add Import Path
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../Core'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../'))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../'))
+
+
+# Refer to CoreDefine.py
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+from CoreDefine import *
+
+
+# IMPORT CORE
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+from Core.CommonUse     import *
+
+
+# Installed Library - QT CORE
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+from qt_core            import *
+
+
+# UI
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+from .ui_main            import Ui_MainWindow
+
+
+# ProgramList
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+ProgramList =   [   'AnalysisAttribute',
+                    'SliceImage',
+                    'MakeClassSource',
+                    'ExtractAnnotation',
+                    'JoinPath',
+                ]
+
+# ProgramInformationList
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+DetailList  =   [   '주어진 자료를 분석하고 엑셀 및 그래프로 출력해주는 프로그램',
+                    '이미지 폴더를 받아서, COMMON, HEAD, UPPER, LOWER 기준에 맞게 잘라주는 프로그램',
+                    '주어진 기준에 맞게 받은 CvatXml 파일 및 ImgName 들을 MakeClass 해주는 프로그램',
+                    'Annotation들을 기준에 맞게 추출하거나 스플릿하는 프로그램',
+                    'ImgList들의 앞에 일괄적으로 경로를 붙여주는 프로그램',
+                ]
+
+# ChoiceProgramUI Class
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+class ChoiceProgramUI(QMainWindow):
+    def __init__(self, QApp=None):
+        super().__init__()
+
+        self.app = QApp
+        self.res = 'EXIT'
+
+        self.programNameList    = []
+        self.infoDict           = {}
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.initialize()
+
+    def initialize(self):
+        self.setProgramNameList()
+        self.syncComboBoxToProgramList()
+        self.setInfoDict()
+        self.ui.detailLabel.setText(self.getProgramInformation())
+
+        self.ui.selectProgramComboBox.currentTextChanged.connect(self.syncDetailLabelToComboBox)
+        self.ui.selectProgramBtn.clicked.connect(self.selectDone)
+
+    def setInfoDict(self):
+        for idx, eachValue in enumerate(ProgramList):
+            self.infoDict[eachValue] = DetailList[idx]
+
+    def getProgramInformation(self):
+        return self.infoDict[self.ui.selectProgramComboBox.currentText()]
+
+
+    def setProgramNameList(self):
+        for eachValue in ProgramList:
+            self.programNameList.append(eachValue)
+
+    def syncComboBoxToProgramList(self):
+        self.ui.selectProgramComboBox.addItems(self.programNameList)
+
+    def syncDetailLabelToComboBox(self):
+        self.ui.detailLabel.setText(self.getProgramInformation())
+
+    def selectDone(self):
+        self.res = self.ui.selectProgramComboBox.currentText()
+        QCoreApplication.instance().quit()
+
+    def run(self):
+        self.res = 'EXIT'
+        self.show()
+        self.app.exec()
+        return self.res
---
+++ UI/ChoiceProgramUI/ui_main.py
@@ -0,0 +1,41 @@
+# Installed Library
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+from PyQt6 import QtCore, QtGui, QtWidgets
+
+
+# VAR DEFINE
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+WINDOW_WIDTH    = 800
+WINDOW_HEIGHT   = 80
+
+# Ui_MainWindow Class
+# -*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow:QtWidgets.QMainWindow):
+        MainWindow.setObjectName('MainWindow')
+        MainWindow.setWindowTitle('Select RunProgram')
+        MainWindow.resize(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralwidget")
+
+        self.selectProgramComboBox  = QtWidgets.QComboBox(MainWindow)
+        self.selectProgramBtn       = QtWidgets.QPushButton('Select', MainWindow)
+
+        self.detailGroupBox         = QtWidgets.QGroupBox('Information', MainWindow)
+        self.detailLabel            = QtWidgets.QLabel(MainWindow)
+
+        self.upper_H_Layout = QtWidgets.QHBoxLayout()
+        self.upper_H_Layout.addWidget(self.selectProgramComboBox, 10)
+        self.upper_H_Layout.addWidget(self.selectProgramBtn, 1)
+
+        self.lower_H_Layout = QtWidgets.QHBoxLayout()
+        self.lower_H_Layout.addWidget(self.detailLabel)
+        self.detailGroupBox.setLayout(self.lower_H_Layout)
+
+        self.mainLayout = QtWidgets.QVBoxLayout()
+        self.mainLayout.addLayout(self.upper_H_Layout, 1)
+        self.mainLayout.addWidget(self.detailGroupBox, 1)
+
+        self.centralWidget.setLayout(self.mainLayout)
+        MainWindow.setCentralWidget(self.centralWidget)
